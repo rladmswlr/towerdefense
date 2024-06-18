760bb319781e6e8e8fc844c1d642190b4a491a5c
KEastWiseman <2ehgus6260@gmail.com>
2024-06-18 17:21:41 +0900

    ✨ update , implement authorization middleware

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -10,12 +10,12 @@ router.post('/sign-up', async(req,res,next)=>{
     const {id, password, paswrodconfirm } = req.body;
     const isExistUser = await prisma.user.findFirst({
         where:{
-            userId,
+            id,
         },
     });
     if(isExistUser){
-        return res.status(409).json({message:'이미 존재하는 아이디 입니다'}); // status code 409 conflict
-    }else if(!IDRULES.test(userId)){
+        return res.status(409).json({message:'이미 존재하는 아이디 입니다'});
+    }else if(!IDRULES.test(id)){
         return res.status(400).json({message:'아이디는 영어 소문자와 숫자 조합으로 구성되어야 합니다'});
     }else if(password.length<6){
         return res.status(400).json({message:`비밀번호는 최소 6자 이상이여야 합니다`});
@@ -25,15 +25,15 @@ router.post('/sign-up', async(req,res,next)=>{
     const hashedPassword = await bcrypt.hash(password,saltRounds);
     // Create new user
     await prisma.users.create({
-        data:{userId,password:hashedPassword},
+        data:{id,password:hashedPassword},
     });
 
     return res.status(201).json({message:'회원가입이 완료되었습니다'});
 })
 
 router.post('/sign-in', async(req, res, next)=>{
-    const { userId, password } = req.body;
-    const user = await prisma.users.findFirst({where : {userId}});
+    const { id, password } = req.body;
+    const user = await prisma.users.findFirst({where : {id}});
     if(!user){
         return res.status(401).json({message: '존재하지 않는 아이디입니다'});
     }
@@ -47,6 +47,7 @@ router.post('/sign-in', async(req, res, next)=>{
     );
 
     res.setHeader('Authorization',`Bearer ${token}`)
+
     return res.status(200).json({message:'로그인 성공'});
 })
 
